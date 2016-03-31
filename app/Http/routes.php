3a723fbb4493f96070c8a9ea6d40fb9204469c7a
mihaigeorge <mihaigeorge.c@gmail.com>
2016-03-31 11:58:09 +0300

    Updated Allowed API Methods

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE');
 /*
 |--------------------------------------------------------------------------
 | Routes File
